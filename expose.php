b75cb15a3a887dc7f1d8d1439c807698df695fa2
Parvez <parvez@themexpert.com>
2014-02-05 17:16:00 +0600

    Module customizer option added

--- expose.php
+++ expose.php
@@ -31,11 +31,46 @@ class plgSystemExpose extends JPlugin{
                 $form->loadFile('params', false);
             }
 
-            if($form->getName() == 'com_templates.style')
+            if( $form->getName() == 'com_templates.style' || 
+                $form->getName() == 'com_modules.module' )
             {
                 // Load Library language file
                 $lang = JFactory::getLanguage();
                 $lang->load('lib_expose', JPATH_SITE);
+            }
+
+            // Load module customizer form.
+            // --------------------------------
+            if (in_array(JRequest::getCmd('option'), array(
+                'com_advancedmodules',
+                'com_contenttemplater',
+                'com_nonumbermanager',
+                'com_rereplacer',
+                'com_snippets',
+                'com_modules',
+                'com_poweradmin',
+            ))) {
+                // Get template name
+                $db = JFactory::getDBO();
+                $query = $db->getQuery(true)
+                        ->select('template')
+                        ->from('#__template_styles')
+                        ->where('client_id = 0 AND home = 1');
+                $db->setQuery($query);
+                $template = $db->loadResult(); 
+
+                // Set path
+                $file           = 'modules_style';
+                $templatePath   = JPATH_ROOT . '/templates/' . $template . '/html/';
+                $finalPath      = JPATH_LIBRARIES . '/expose/admin/' ;
+                
+                if( file_exists( $templatePath . $file . '.xml' ) )
+                {
+                    $finalPath = $templatePath;
+                }
+
+                JForm::addFormPath( $finalPath );
+                $form->loadFile( $file, false);
             }
         }
 
